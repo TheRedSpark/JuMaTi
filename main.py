6b7a8751553ea44d13beef189ffc2fa824e4732f
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-11-30 19:09:53 +0100

    Ampelschaltung Übergang fertig

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import time
 
 led = Pin("LED", Pin.OUT)
 rotAuto = Pin(2, Pin.OUT)
-gruenAuto = Pin(4, Pin.OUT)
-gelbAuto = Pin(6, Pin.OUT)
+gruenAuto = Pin(6, Pin.OUT)
+gelbAuto = Pin(4, Pin.OUT)
+rotMensch = Pin(21, Pin.OUT)
+gruenMensch = Pin(19, Pin.OUT)
 taster = Pin(12, Pin.IN, Pin.PULL_DOWN)
 
 led(0)
@@ -13,24 +15,47 @@ time.sleep(0.1)
 gruenAuto(0)
 time.sleep(0.1)
 gelbAuto(0)
+time.sleep(0.1)
+gruenMensch(0)
+time.sleep(0.1)
+rotMensch(0)
+
+haltewunsch = False
+
+
+def normal():
+    gruenAuto(1)
+    time.sleep(0.1)
+    rotMensch(1)
+
+
+def fuSSganger():
+    gelbAuto(1)
+    gruenAuto(0)
+    time.sleep_ms(300)
+    rotAuto(1)
+    gelbAuto(0)
+    time.sleep(1)
+    rotMensch(0)
+    gruenMensch(1)
+
+    time.sleep(2)  # The Grüne Zeit
+
+    gruenMensch(0)
+    rotMensch(1)
+    time.sleep_ms(300)
+    gelbAuto(1)
+    time.sleep_ms(500)
+    rotAuto(0)
+    gelbAuto(0)
+    gruenAuto(1)
+
+
+normal()
 
 while True:
     if taster.value() == 1:
-        led(1)
-        time.sleep(0.5)
-        led(0)
+        fuSSganger()
 
     else:
-        rotAuto(1)
-        time.sleep(0.2)
-        gruenAuto(1)
-        time.sleep(0.2)
-        gelbAuto(1)
-
-        time.sleep(1)
-
-        rotAuto(0)
-        time.sleep(0.2)
-        gruenAuto(0)
-        time.sleep(0.2)
-        gelbAuto(0)
+        pass
